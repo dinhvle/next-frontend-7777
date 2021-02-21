bf97d7e4bc33158b08ce9fbc9e7c9280d2fd2318
Dinh Le <dinh.v.le@gmail.com>
2021-02-20 23:20:14 -0600

    basic layout for pages

---
+++ components/Page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h2>Page Component</h2>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Page from '../components/Page';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <Page />
+      <p>IndexPage</p>
+    </div>
+  );
+}
