541e4c76a112084055145b8ec2e9660327cf9267
Dinh Le <dinh.v.le@gmail.com>
2021-03-02 22:51:12 -0600

    create Product from form submit using gql mutations

--- components/CreateProduct.js
+++ components/CreateProduct.js
@@ -1,6 +1,33 @@
-import { useState } from 'react';
+import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
 import Form from '../components/styles/Form';
+import DisplayError from '../components/ErrorMessage';
+import { useMutation } from '@apollo/client';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # which variables are getting passed in? And what types are they
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "DRAFT"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
@@ -10,14 +37,23 @@ export default function CreateProduct() {
     description: 'Only the greatest bag!',
   });
 
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
-        console.log(inputs);
+        // Submit the inputfields to the backend:
+        await createProduct();
+        clearForm();
       }}
     >
-      <fieldset>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
           <input
