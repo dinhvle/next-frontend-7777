1a272002312ae58b5641d9b3deb1e969f758250e
Dinh Le <dinh.v.le@gmail.com>
2021-04-21 16:26:57 -0500

    getting ready for vercel

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `http://api.onepointfiver.com/api/graphql`;
-export const prodEndpoint = `http://api.onepointfiver.com/api/graphql`;
+export const endpoint = `https://api.onepointfiver.com/api/graphql`;
+export const prodEndpoint = `https://api.onepointfiver.com/api/graphql`;
 export const perPage = 4;
